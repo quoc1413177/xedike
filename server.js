75019870ca2cb243cb9f3a3ba74bab338a5a1594
quoc1413177 <nguyenminhquoc1413177@gmail.com>
2019-04-12 19:50:22 +0700

    init server branch feature

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const express = require('express')
+
+/** Init Server */
+const app = express();
+const port = process.env.PORT || 5000
+app.listen(port, () =>{
+    console.log("Server Connected !!!")
+})
