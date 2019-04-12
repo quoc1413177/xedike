c7899b1344ce1236b003242facdd55d7c31b246f
quoc1413177 <nguyenminhquoc1413177@gmail.com>
2019-04-12 20:04:41 +0700

    devMegre

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+/**Connect DB */
+const MONGO_URI = 'mongodb://localhost:27017/xedike'
+mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+    .then(console.log('Connected to FB !'))
+    .catch(console.log)
 
 /** Init Server */
 const app = express();
